1f014f326dc8fe2d0134dcea3f481e2f6dbb1f47
Chelsea Wilkinson <mail@chelseawilkinson.me>
2024-11-30 23:10:28 +1100

    cleanup

--- flake.nix
+++ flake.nix
@@ -15,36 +15,15 @@
     let
       inherit (nixpkgs.lib) nixosSystem;
       basic-config = { pkgs, lib, ... }: {
-        # bcm2711 for rpi 3, 3+, 4, zero 2 w
-        # bcm2712 for rpi 5
-        # See the docs at:
-        # https://www.raspberrypi.com/documentation/computers/linux_kernel.html#native-build-configuration
         raspberry-pi-nix.board = "bcm2712";
-        time.timeZone = "America/New_York";
+        time.timeZone = "Australia/Melbourne";
         users.users.root.initialPassword = "root";
         networking = {
-          hostName = "basic-example";
+          hostName = "nixos";
           useDHCP = false;
           interfaces = { wlan0.useDHCP = true; };
         };
-        environment.systemPackages = with pkgs; [ bluez bluez-tools ];
-        hardware = {
-          bluetooth.enable = true;
-          raspberry-pi = {
-            config = {
-              all = {
-                base-dt-params = {
-                  # enable autoprobing of bluetooth driver
-                  # https://github.com/raspberrypi/linux/blob/c8c99191e1419062ac8b668956d19e788865912a/arch/arm/boot/dts/overlays/README#L222-L224
-                  krnbt = {
-                    enable = true;
-                    value = "on";
-                  };
-                };
-              };
-            };
-          };
-        };
+        environment.systemPackages = with pkgs; [ ];
       };
 
     in
