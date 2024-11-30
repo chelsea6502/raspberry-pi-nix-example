3768b2598eb2b2d59cd89c0fabd9fbb58e0869c8
Chelsea Wilkinson <122856043+chelsea6502@users.noreply.github.com>
2024-11-30 23:06:05 +1100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
         # bcm2712 for rpi 5
         # See the docs at:
         # https://www.raspberrypi.com/documentation/computers/linux_kernel.html#native-build-configuration
-        raspberry-pi-nix.board = "bcm2711";
+        raspberry-pi-nix.board = "bcm2712";
         time.timeZone = "America/New_York";
         users.users.root.initialPassword = "root";
         networking = {
